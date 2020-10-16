eb87c3e587d8047f90ae1c4643fea3cdb03014e0
Samuel <samuel.dubuis@netplus.ch>
2020-10-16 13:40:18 +0200

    .

--- deliberative/src/template/DeliberativeAgent.java
+++ deliberative/src/template/DeliberativeAgent.java
@@ -169,7 +169,42 @@ public class DeliberativeAgent implements DeliberativeBehavior {
 	}
 	
 	public Node ASTAR(Vehicle vehicle, TaskSet tasks) {
-		// TODO
+		// TODO a checker
+		City currentLoc = vehicle.getCurrentCity();
+		Plan plan = new Plan(currentLoc);
+		
+		State firstState = new State(currentLoc, tasks, vehicle.getCurrentTasks());
+
+		List<Node> Q = new ArrayList<Node>();
+		Q.add(new Node(null, null, firstState));
+		
+		Map<State, Double> C = new HashMap<State, Double>();
+		
+		while (!Q.isEmpty()) {
+			Node n = Q.get(0);
+			Q.remove(0);
+			
+			if (n.getState().isFinalState()) {
+				return n;
+			}
+			
+			if (!C.containsKey(n.getState()) || C.get(n.getState()) > n.f() ) {
+				// if n not member of C or has lower cost than its own copy in C
+				// add n to C
+				// append successors of n to Q
+				
+				C.put(n.getState(), n.f());
+				List<Node> S = n.getSuccessors(vehicle);
+				
+				S.sort((node1, node2) -> Double.compare(node1.f(), node2.f())); // successors of n are sorted 
+				Q.addAll(S);													// merged with Q list
+				Q.sort((node1, node2) -> Double.compare(node1.f(), node2.f())); // Q list is also sorted for better use
+			}
+		}
+		
+		// if Q is empty return failure
+		System.out.println("Failed");
+		return null;
 	}
 	
 }
